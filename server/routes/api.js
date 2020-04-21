2b4c57ace589c0e3c2eb38b0b4a5e9964bb4d0f2
benjaminfester <benfesterh@gmail.com>
2020-04-21 15:47:26 +0200

    made into angular project

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
 const Meal = require('../models/meal')
 const mongoose = require('mongoose')
+const bcryptjs = require('bcryptjs')
+const saltRounds = 10
 
 const db = "mongodb+srv://benjamin:yankee92@cluster0-j4bhw.mongodb.net/unseenbiodb?retryWrites=true&w=majority"
 
@@ -18,15 +21,90 @@ router.get('/', (req, res) => {
     res.send('From api ')
 })
 
+router.post('/register', (req, res) => {
+    bcryptjs.hash(req.body.password, saltRounds, function(err, hash) {
+        try{
+            var user = new User({email: req.body.email, password: hash})
+            if(req.body.email == null || req.body.email == '' || req.body.password == null || req.body.password == '') {
+                res.send('Ensure email and password were provided')
+            } else {
+                user.save((err) => {
+                    if(err) {
+                        res.send(err + ' Email already exists')
+                    } else {
+                        const token = jwt.sign({
+                            user: req.body.email
+                        },
+                        'secretKey',
+                        {
+                            expiresIn: "1h"
+                        }
+                        )
+                        res.status(200).json({
+                            message: 'User with email: ' + req.body.email + ' was created.',
+                            token: token
+                        })
+                    }
+                })
+            }
+        } catch(err) {
+            console.log(err)
+            res.status(400).send(err)
+        }
+    }) 
+})
+
+router.post('/login', (req, res) => {
+    try {
+        var user = User.findOne({email: req.body.email}).then(function(user) {
+            if(!user) {
+                return res.status(401).json({
+                    message: 'Authentication failed. Wrong email or password'
+                })
+            } else {
+                bcryptjs.compare(req.body.password, user.password, function(err, result) {
+                    if(result) {
+                        const token = jwt.sign({
+                            email: req.body.email,
+                        },
+                        'secretKey',
+                        {
+                            expiresIn: "1h"
+                        }
+                        )
+                        return res.status(200).json({
+                            message: req.body.email + ' is now logged in.',
+                            token: token    
+                        })
+
+                    } else {
+                        res.status(401).json({
+                            message: 'Authentication failed. Wrong email or password',
+                            error: err
+                        })
+                    }
+                })
+            }
+        })
+    } catch(err) {
+        console.log(err)
+    }
+})
+
 router.post('/dietdiary', (req, res) => {
     let meal = new Meal({title: req.body.title, content: req.body.content, calories: req.body.calories})
-    meal.save((error, addedMeal) => {
-        if(error) {
-            console.log(error)
-        } else {
-            res.status(200).send(addedMeal)
-        }
-    })
+    if(req.body.title == null || req.body.title == '' || req.body.calories == '' || req.body.calories == null) {
+        res.send('Make sure to type in title and calories.')
+    } else {
+
+        meal.save((error, addedMeal) => {
+            if(error) {
+                console.log(error)
+            } else {
+                res.status(200).send(addedMeal)
+            }
+        })
+    }
 })
 
 router.get('/dietdiary', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 const port = 3000
 const api = require('./routes/api')
 const app = express()
 
-
+app.use(cors())
 app.use(bodyparser.json())
 
 app.use('/api', api)
-
-app.get('/', (req, res) => {
-    res.send('From server')
-})
 
 app.listen(port, function() {
     console.log('Server running on port ' + port + '...') 
